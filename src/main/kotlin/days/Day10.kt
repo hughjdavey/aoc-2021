7b48afb3204ab7e858cad1b4bc404da6dfb0071b
hughjdavey <9026053+hughjdavey@users.noreply.github.com>
2021-12-10 17:17:47 +0000

    Day 10 refactor
    Rename isCorrupt and simplify logic
    Simplify getMissing logic and make more functional
    Rename and/or simplify Char extension methods

--- src/main/kotlin/days/Day10.kt
+++ src/main/kotlin/days/Day10.kt
@@ -5,85 +5,37 @@ import java.util.Stack
 class Day10 : Day(10) {
 
     override fun partOne(): Any {
-        return inputList.mapNotNull { isCorrupted(it) }.sumOf { it.getCorruptScore() }
+        return inputList.mapNotNull { getFirstIllegalChar(it) }.sumOf { it.getIllegalScore() }
     }
 
     override fun partTwo(): Any {
-        val missing = inputList.filter { isCorrupted(it) == null }
+        val missing = inputList.filter { getFirstIllegalChar(it) == null }
             .map { getMissing(it).fold(0L) { score, c -> score * 5 + c.getIncompleteScore() } }
         return missing.sorted()[(missing.size / 2)]
     }
 
-    private fun Char.getCorruptScore(): Int {
-        return when (this) {
-            ')' -> 3
-            ']' -> 57
-            '}' -> 1197
-            '>' -> 25137
-            else -> 0
-        }
-    }
-
-    private fun Char.getIncompleteScore(): Long {
-        return when (this) {
-            ')' -> 1
-            ']' -> 2
-            '}' -> 3
-            '>' -> 4
-            else -> 0
-        }
-    }
-
-    private fun Char.getOpposite(): Char {
-        return when (this) {
-            '(' -> ')'
-            ')' -> '('
-            '[' -> ']'
-            ']' -> '['
-            '{' -> '}'
-            '}' -> '{'
-            '<' -> '>'
-            '>' -> '<'
-            else -> this
-        }
-    }
-
     private fun getMissing(s: String): String {
-        val unmatched = Stack<Char>()
-        for (char in s) {
-            if (listOf('(', '[', '{', '<').contains(char)) {
-                unmatched.push(char)
-                continue
-            }
-
-            val lastUnmatched = unmatched.peek()
-            if (lastUnmatched == '(' && char == ')' || lastUnmatched == '[' && char == ']' || lastUnmatched == '{' && char == '}' || lastUnmatched == '<' && char == '>') {
-                unmatched.pop()
-            }
-        }
-        return unmatched.toList().map { it.getOpposite() }.joinToString("").reversed()
+        return s.fold(Stack<Char>()) { unmatched, char ->
+            if (listOf('(', '[', '{', '<').contains(char)) unmatched.push(char) else unmatched.pop()
+            unmatched
+        }.toList().map { it.getMirror() }.joinToString("").reversed()
     }
 
-    private fun isCorrupted(s: String): Char? {
+    private fun getFirstIllegalChar(s: String): Char? {
         val unmatched = Stack<Char>()
         for (char in s) {
-            if (listOf('(', '[', '{', '<').contains(char)) {
-                unmatched.push(char)
-                continue
-            }
-
-            if (listOf(')', ']', '}', '>').contains(char) && unmatched.empty()) {
-                return char
-            }
-
-            val lastUnmatched = unmatched.peek()
-            if (lastUnmatched == '(' && char == ')' || lastUnmatched == '[' && char == ']' || lastUnmatched == '{' && char == '}' || lastUnmatched == '<' && char == '>') {
-                unmatched.pop()
-            }
-            else {
-                return char
+            when {
+                listOf('(', '[', '{', '<').contains(char) -> unmatched.push(char)
+                unmatched.isNotEmpty() && listOf("()", "[]", "{}", "<>").contains("${unmatched.peek()}$char") -> unmatched.pop()
+                else -> return char
             }
         }
         return null
     }
+
+    private fun Char.getIllegalScore(): Int = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137).getOrDefault(this, 0)
+
+    private fun Char.getIncompleteScore(): Long = mapOf(')' to 1L, ']' to 2L, '}' to 3L, '>' to 4L).getOrDefault(this, 0)
+
+    private fun Char.getMirror(): Char = mapOf('(' to ')', ')' to '(', '[' to ']', ']' to '[', '{' to '}', '}' to '{', '<' to '>', '>' to '<').getOrDefault(this, this)
 }
